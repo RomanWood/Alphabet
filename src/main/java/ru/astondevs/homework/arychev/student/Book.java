c49d097dfb0359c045d7885c96ae8ffce406bb76
Roman <roman.arychev@gmail.com>
2026-06-23 12:15:44 +0300

    HM-2 pr fixes

--- src/main/java/ru/astondevs/homework/arychev/student/Book.java
+++ src/main/java/ru/astondevs/homework/arychev/student/Book.java
@@ -1,60 +1,17 @@
 package ru.astondevs.homework.arychev.student;
 
-import lombok.Getter;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Objects;
 
-public class Book implements Serializable, Comparable<Book> {
+public record Book(String name, String author, LocalDate releaseDate,
+                   int totalPages) implements Serializable, Comparable<Book> {
 
     @Serial
     private static final long serialVersionUID = 100L;
 
-    private final String name;
-    private final String author;
-    @Getter
-    private final LocalDate releaseDate;
-    @Getter
-    private final int totalPages;
-
-    public Book(String name, String author, LocalDate yearOfRelease, int totalPages) {
-        this.name = name;
-        this.author = author;
-        this.releaseDate = yearOfRelease;
-        this.totalPages = totalPages;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                ", yearOfRelease=" + releaseDate +
-                ", totalPages=" + totalPages +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true; // Проверка на один и тот же объект
-        if (obj == null || getClass() != obj.getClass()) return false; // Проверка типа и null
-        Book book = (Book) obj;
-        return Objects.equals(name, book.name)
-                && Objects.equals(author, book.author)
-                && Objects.equals(releaseDate, book.releaseDate)
-                && Objects.equals(totalPages, book.totalPages); // Сравнение полей
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, author, releaseDate, totalPages);
-    }
-
-
     @Override
     public int compareTo(Book o) {
-        return Integer.compare(this.totalPages, o.getTotalPages());
+        return Integer.compare(this.totalPages, o.totalPages());
     }
 }
--- src/main/java/ru/astondevs/homework/arychev/student/Student.java
+++ src/main/java/ru/astondevs/homework/arychev/student/Student.java
@@ -1,30 +1,12 @@
 package ru.astondevs.homework.arychev.student;
 
-import lombok.Getter;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
-public class Student implements Serializable {
+public record Student(String fio, List<Book> books) implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 100L;
 
-    @Getter
-    private final String fio;
-    @Getter
-    private final List<Book> books;
-
-    public Student(String fio, List<Book> books) {
-        this.fio = fio;
-        this.books = books;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "fio='" + fio + '\'' +
-                '}';
-    }
 }
